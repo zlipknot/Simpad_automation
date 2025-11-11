f0788f87c84f840176383bae8ac215fc39f71fb8
zlipknot <zlipknot.misha@gmail.com>
2025-11-11 11:23:18 +0100

    Add small unit test

---
+++ tests/test_text_match_unit.py
@@ -0,0 +1,10 @@
+from simpad_automation.core.verify import normalize_text, compare_tokens
+
+def test_normalize_basic():
+    assert normalize_text("  Unable  to  retrieve  technical  information ") \
+           == "unable to retrieve technical information"
+
+def test_compare_tokens_fuzzy_ok():
+    target = "Unable to retrieve technical information"
+    ocr = "Unable te retrieve technicalinformation"
+    assert compare_tokens(target, ocr, ok_ratio=0.7) is True
